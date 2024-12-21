aca8ac62d4857d5a4d63f558bdb4c01af0767e5b
sabariyuhendh <yuhendhran@gmail.com>
2024-12-21 09:26:54 +0530

    minor

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,35 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div
+    className="hero min-h-screen"
+    style={{
+      backgroundImage: "url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)",
+    }}>
+    <div className="hero-overlay bg-opacity-60"></div>
+    <div className="hero-content text-neutral-content text-center">
+      <div className="navbar">
+      
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+      <div className="max-w-md">
+        <h1 className="mb-5 text-5xl font-bold">Hello there make an appointment</h1>
+        <p className="mb-5">
+        At Groomz, we believe in giving back.
+We regularly support local charities and
+events to spread beauty beyond our doors.
         </p>
+        <button className="btn btn-primary">Get Started</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
+  </div>
   )
+
 }
 
 export default App
