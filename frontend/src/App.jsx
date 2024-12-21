7dacaea901f2a963aa5ca8df59d28f1f81c5c19f
sabariyuhendh <yuhendhran@gmail.com>
2024-12-21 11:32:25 +0530

    major

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,27 +7,60 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
+
     <div
-    className="hero min-h-screen"
-    style={{
-      backgroundImage: "url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)",
-    }}>
-    <div className="hero-overlay bg-opacity-60"></div>
-    <div className="hero-content text-neutral-content text-center">
-      <div className="navbar">
-      
-      </div>
-      <div className="max-w-md">
-        <h1 className="mb-5 text-5xl font-bold">Hello there make an appointment</h1>
-        <p className="mb-5">
-        At Groomz, we believe in giving back.
-We regularly support local charities and
-events to spread beauty beyond our doors.
-        </p>
-        <button className="btn btn-primary">Get Started</button>
+
+      className="hero min-h-screen"
+      style={{
+        backgroundImage: "url(https://images.unsplash.com/photo-1491466424936-e304919aada7?q=80&w=1769&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D.webp)",
+      }}>
+      {
+        <nav className="bg-white border-gray-200 dark:bg-gray-900 fixed top-0 left-0 w-full z-10">
+          <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+            <a href="https://google.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
+              <img src="https://.svg" className="h-8" alt="Logo" />
+              <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Groomz</span>
+            </a>
+            <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+              <span className="sr-only">Open main menu</span>
+              <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
+              </svg>
+            </button>
+            <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+              <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                <li>
+                  <a href="#" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</a>
+                </li>
+                <li>
+                  <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
+                </li>
+                <li>
+                  <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</a>
+                </li>
+                <li>
+                  <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">History</a>
+                </li>
+                <li>
+                  <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      }
+      <div className="hero-content text-neutral-content text-center">
+        <div className="max-w-md">
+          <h1 className="mb-5 text-5xl font-bold text-white">Hello there make an appointment</h1>
+          <p className="mb-5 text-white">
+            At Groomz, we believe in giving back.
+            We regularly support local charities and
+            events to spread beauty beyond our doors.
+          </p>
+          <button className="btn btn-primary">Get Started</button>
+        </div>
       </div>
     </div>
-  </div>
   )
 
 }
